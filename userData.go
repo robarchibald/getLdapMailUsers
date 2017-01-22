d4cfffc601bc40475c7ba243a2724356a38a61e2
Rob Archibald <rob.archibald@endfirst.com>
2017-01-22 09:46:46 -0800

    check both homedirectory and mailfolder for data

--- userData.go
+++ userData.go
@@ -15,12 +15,13 @@ type UserData struct {
 }
 
 type ldapData struct {
-	UID          []string
-	UserPassword []string
-	UIDNumber    []string
-	GIDNumber    []string
-	MailFolder   []string
-	MailQuota    []string
+	UID           []string
+	UserPassword  []string
+	UIDNumber     []string
+	GIDNumber     []string
+	HomeDirectory []string
+	MailFolder    []string
+	MailQuota     []string
 }
 
 func newUserData(data *ldapData) *UserData {
@@ -33,6 +34,9 @@ func newUserData(data *ldapData) *UserData {
 	}
 	if len(data.UIDNumber) != 0 {
 		uidNumber = data.UIDNumber[0]
+	}
+	if len(data.HomeDirectory) != 0 {
+		mailFolder = data.HomeDirectory[0]
 	}
 	if len(data.MailFolder) != 0 {
 		mailFolder = data.MailFolder[0]
